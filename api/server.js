151e4f54f6223544e9018a0e05d900862bc5acc7
Clay Loftus <clayton.m.loftus@gmail.com>
2023-10-13 22:14:00 -0500

    Move db setup to its own module

--- api/server.js
+++ api/server.js
@@ -1,18 +1,10 @@
 const express = require('express')
 require('dotenv').config();
-const {Pool} = require('pg')
-
+const initializeDatabase = require('./utilities/db_setup')
 const app = express()
 const port = process.env.PORT
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-})
-
+initializeDatabase
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
