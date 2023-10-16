8ac2ee60c68fea98166fefd9ba0d2f95dbe90a05
Clay Loftus <clayton.m.loftus@gmail.com>
2023-10-16 17:13:06 -0500

    Replace pg with sequelize

---
+++ api/config/db.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST
+  }
+)
+
+const testDbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = { sequelize, testDbConnection };
