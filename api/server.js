34e69b6f330584ed2efd18ae536772e4160ff6d3
Clay Loftus <clayton.m.loftus@gmail.com>
2023-10-16 17:12:47 -0500

    Add sequelize connection

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 require('dotenv').config();
-const initializeDatabase = require('./utilities/db_setup')
+const { testDbConnection } = require('./config/db')
 const app = express()
 const port = process.env.PORT
 
-initializeDatabase
+testDbConnection()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
