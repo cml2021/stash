1c350aac26059f3c93fe9cc7c43cde034e96187e
Clay Loftus <clayton.m.loftus@gmail.com>
2023-10-19 14:29:53 -0500

    Clean up db initialization

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 require('dotenv').config();
-const { testDbConnection } = require('./config/db')
+const { sequelize, testDatabaseConnection } = require('./config/db')
 const app = express()
 const port = process.env.PORT
 
-testDbConnection()
+try { 
+  testDatabaseConnection();   
+} catch (error) {
+  console.log(error);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
