c8ba26082ba1fcccbbf9ea65f3f8d3cb20bf49dd
Clay Loftus <clayton.m.loftus@gmail.com>
2023-10-23 14:33:26 -0600

    Move routes to own directory

--- api/server.js
+++ api/server.js
@@ -1,57 +1,27 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
 require('dotenv').config();
 
 const { db, testDatabaseConnection } = require('./models/index');
-const { ITEM_TYPES } = require('./constants')
-const { createItem } = require('./managers/item_manager')
+const items = require('./routes/items')
 
 const app = express();
+const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT;
-
+// Connect to database
 try { 
   testDatabaseConnection();   
 } catch (error) {
   console.log(error);
 }
 
+// Routes
+app.use('/items', items);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-app.post('/items', asyncHandler(async (req, res) => {
-  const data = req.body;
-  const name = data['name'];
-  const type = data['type'];
-
-  /* Validate request params */
-  if (name == null || name == '') {
-    res.status(400)
-    .send('Item name cannot be empty')
-  } else if (typeof(name) != 'string') {
-    res.status(400)
-    .send('Item name must be a string')
-  } else if (name.length > 255) {
-    res.status(400)
-    .send('Item name must be 255 characters or less')
-  } else if (!ITEM_TYPES.includes(type)) {
-    res.status(400)
-    .send('Item type not recognized')
-  }
-
-  // /* Create new Item instance */
-  const response = await createItem(name, type);
-  if (response instanceof Error) {
-    res.status(500)
-    .send('Item could not be created')
-  } else {
-    res.status(201)
-    .json(response.dataValues)
-  }
-}))
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`)
