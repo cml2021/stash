b6b53357a4a1e4765453ee748f53f140a93222f7
cml2021 <clayton.m.loftus@gmail.com>
2023-10-24 14:45:26 -0600

    Add tests on create item method

---
+++ api/managers/__tests__/items.tests.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+const supertest = require('supertest');
+const { app } = require('../../app');
+const { db } = require('../../models/index');
+const { createItem } = require('../items');
+
+describe('test the createItem method', () => {
+  const testDb = db;
+  console.log(testDb);
+
+  beforeAll(async () => {
+    await testDb.sequelize.sync({ force: true });
+  });
+
+  it('should create a new Item instance', async () => {
+    const name = 'The Road';
+    const type = 'Book';
+
+    const item = await createItem(name, type);
+    expect(item.dataValues.name).toBe(name);
+    expect(item.dataValues.type).toBe(type);
+  });
+
+  afterAll(async () => {
+    await testDb.sequelize.close();
+  });
+});
