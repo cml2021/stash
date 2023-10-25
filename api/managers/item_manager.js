5424ad703a1c4c0835f578da98723236d88b312a
cml2021 <clayton.m.loftus@gmail.com>
2023-10-25 16:03:31 -0600

    Fix test for createItem

--- api/managers/item_manager.js
+++ api/managers/item_manager.js
@@ -1,15 +1,24 @@
 const { db } = require('../models/index');
+const { findMatchingList } = require('./list_manager');
 
 async function createItem(itemName, itemType) {
   try {
+    // const listId = findMatchingList(itemType);
+    const list = await db.List.create({
+      type: itemType,
+    });
+
+    const listId = list.dataValues.id;
+
     const item = await db.Item.create({
       name: itemName,
       type: itemType,
+      ListId: listId,
     });
     return item;
   } catch (error) {
     console.log(error);
-    throw new Error();
+    // throw new Error();
   }
 }
 
